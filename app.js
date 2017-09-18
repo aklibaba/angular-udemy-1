2ed2254daa714e3ca5666122b2e136449cd66dae
Aleksander <aleksander@direktpoint.pl>
2017-09-18 23:43:53 +0200

    ajax example

--- app.js
+++ app.js
@@ -14,11 +14,22 @@ myApp.controller('mainController', [ '$log', '$scope', '$timeout', '$filter',
       });
     }, 3000);
 
-    $scope.rules = [
-      {rulename: 'a'},
-      {rulename: 'bb'},
-      {rulename: 'ccc'}
-    ];
-    $log.log($scope.rules);
+    $scope.users = [];
+
+    var root = 'https://jsonplaceholder.typicode.com';
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if ( xhr.readyState === 4 && xhr.status === 200 ) {
+        $scope.$apply(function () {
+          $scope.users = JSON.parse(xhr.responseText);
+        })
+      }
+    };
+    xhr.open("GET", root + '/users', true);
+    xhr.send();
 
   } ]);
+
+
+
