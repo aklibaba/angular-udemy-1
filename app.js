301207f9727c9e3fc392b79ad8092d5046eee8b0
Aleksander <aleksander@direktpoint.pl>
2017-09-19 14:54:49 +0200

    start work with $http

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var myApp = angular.module('myApp', [ 'ngMessages', 'ngResource' ]);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', [ '$log', '$scope', '$timeout', '$filter',
-  function ($log, $scope, $timeout, $filter) {
+myApp.controller('mainController', ['$log', '$scope', '$timeout', '$filter', '$http',
+  function ($log, $scope, $timeout, $filter, $http) {
     $scope.nick = 'Michal';
+    $scope.newRules = [];
+    $scope.newRule;
+    $scope.submit = function () {
+      console.log('submitted');
+      $scope.newRules.push($scope.newRule)
+    };
     $scope.lcNick = function () {
       return $filter('lowercase')($scope.nick);
     };
@@ -18,18 +24,35 @@ myApp.controller('mainController', [ '$log', '$scope', '$timeout', '$filter',
 
     var root = 'https://jsonplaceholder.typicode.com';
 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if ( xhr.readyState === 4 && xhr.status === 200 ) {
-        $scope.$apply(function () {
-          $scope.users = JSON.parse(xhr.responseText);
-        })
-      }
-    };
-    xhr.open("GET", root + '/users', true);
-    xhr.send();
+    // traditionalXhr($scope,root);
 
-  } ]);
+    /**
+     * @function angular.$http
+     */
+    $http.get(root + '/users')
+      .then(function (response) {
+        console.log(response.data);
+        $scope.users = response.data;
+      })
+      .catch(function(data, status) {
+        console.log(data);
+        console.log(status);
+      })
+    
+  }]);
+
+function traditionalXhr($scope, root) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      $scope.$apply(function () {
+        $scope.users = JSON.parse(xhr.responseText);
+      })
+    }
+  };
+  xhr.open("GET", root + '/users', true);
+  xhr.send();
+}
 
 
 
